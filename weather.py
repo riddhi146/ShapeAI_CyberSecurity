6b7f32726659b93e28d36b58210132d8c9ce2a93
riddhi146 <83352866+riddhi146@users.noreply.github.com>
2021-06-23 16:35:31 +0530

    Update weather.py

--- weather.py
+++ weather.py
@@ -6,7 +6,6 @@ city_name = input("Enter the name of the city: \n")
 
 comp_api_link = 'https://api.openweathermap.org/data/2.5/weather?q='+city_name+'&appid='+api_key
 api_link = requests.get(comp_api_link)
-#r = requests.get(comp_api_link.format(city_name)).json()
 api_data = api_link.json()
 
 temp_city = ((api_data["main"]["temp"]) - 273.15)
@@ -25,19 +24,18 @@ print('Current wind speed: ',wind_spd,'kmph')
 print("====================================================")
 
 
-# making a list so that i can print the info to a txt
 txtlist = [temp_city,weather_desc,hmdt,wind_spd,date_time]
-#using open() buit-in function to write to a text file
+
 with open("textfile.txt" , mode= 'w' ,encoding= 'cp1252') as f :
-                                     #encoding = utf-8 for linux and cp1252 for win
+                                     
     f.write("------------------------------------------------------------- \n ")
     f.write("Weather Stats for - {}  || {}".format(city_name.upper(), date_time))
     f.write("\n ------------------------------------------------------------- \n")
     f.write("Current temperature is: {:.2f} deg C\n".format(txtlist[0]))
 
-    f.write("{},{} \n".format("Current weather desc  :" ,txtlist[1]))
-    f.write("{},{},{} \n".format("Current Humidity      :",txtlist[2],"%"))
-    f.write("{},{},{} \n".format("Current wind speed    :",txtlist[3],"kmph"))
+    f.write("{},{} \n".format("Current weather desc: " ,txtlist[1]))
+    f.write("{},{},{} \n".format("Current Humidity: ",txtlist[2],"%"))
+    f.write("{},{},{} \n".format("Current wind speed :",txtlist[3],"kmph"))
     f.write("====================================================")
 
 
